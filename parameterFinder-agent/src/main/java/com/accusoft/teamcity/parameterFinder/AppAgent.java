c1d7fadb0f91c4c7180609fe5c64c7775a044a74
matthew7j <matthew7j@aol.com>
2015-06-05 15:00:19 -0400

    Still trying stuff

--- parameterFinder-agent/src/main/java/com/accusoft/teamcity/parameterFinder/AppAgent.java
+++ parameterFinder-agent/src/main/java/com/accusoft/teamcity/parameterFinder/AppAgent.java
@@ -1,4 +1,40 @@
 package com.accusoft.teamcity.parameterFinder;
 
-public class AppAgent {
+import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class AppAgent extends AgentLifeCycleAdapter {
+    public AppAgent() {
+
+    }
+    public void pluginsLoaded () {
+
+        PrintWriter w = null;
+        try {
+            w = new PrintWriter("C:\\Users\\mjones\\Desktop\\file.txt", "UTF-8");
+            w.println("HELLO");
+        }
+        catch (Exception e) {
+            w.println(e.getStackTrace());
+        }
+        finally {
+            w.close();
+        }
+
+        ArrayList<String> regexes = new ArrayList<String>();
+
+        if (System.getProperty("os.name").contains("Windows")) {
+            regexes.clear();
+            regexes.add("Python ([\\d\\.]+)");
+            new ParameterFinder(regexes, "C:\\Python", "-V", "python.exe");
+        }
+        else {
+            regexes.clear();
+            regexes.add("python([\\d\\.]+)");
+            new ParameterFinder(regexes, "/usr/share/python", "-i", "pyversions.py");
+        }
+    }
+
 }
--- parameterFinder-agent/src/main/java/com/accusoft/teamcity/parameterFinder/ParameterFactory.java
+++ parameterFinder-agent/src/main/java/com/accusoft/teamcity/parameterFinder/ParameterFactory.java
@@ -1,6 +1,5 @@
 package com.accusoft.teamcity.parameterFinder;
 
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class ParameterFactory
@@ -10,34 +9,8 @@ public class ParameterFactory
     }
     public static void main(String[] args) {
 
-        PrintWriter w = null;
-        System.err.println("ASKDJNASKJASNDKJASNSAJNJDNASADSNKJSANSJKADSN");
-        try {
-            w = new PrintWriter("C:\\Users\\mjones\\Desktop\\file.txt", "UTF-8");
-            w.println("hi");
-        }
-        catch (Exception e) {
-            w.println(e.getStackTrace());
-        }
-        finally {
-            w.close();
-        }
+        new AppAgent();
 
-        ArrayList<String> regexes = new ArrayList<String>();
-/*
-        regexes.clear();
-        regexes.add("Gradle ([\\d\\.]+)");
-        new ParameterFinder(regexes, "C:\\Program Files\\Gradle", "-version", "gradle.bat");
-*/
-        if (System.getProperty("os.name").contains("Windows")) {
-            regexes.clear();
-            regexes.add("Python ([\\d\\.]+)");
-            new ParameterFinder(regexes, "C:\\Python", "-V", "python.exe");
-        }
-        else {
-            regexes.clear();
-            regexes.add("python([\\d\\.]+)");
-            new ParameterFinder(regexes, "/usr/share/python", "-i", "pyversions.py");
-        }
+
     }
 }
